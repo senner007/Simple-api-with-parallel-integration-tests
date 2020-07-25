e0dab7ed47643c8494f68722ffd23237c3c212f9
nig <nig@beepanalytics.com>
2020-07-25 19:54:50 +0200

    fsync off on test db for performance

--- db_setup.sh
+++ db_setup.sh
@@ -5,4 +5,4 @@ docker stop some-postgres-db || true && docker rm some-postgres-db -f || true
 echo ""
 echo "Start development and test containers:"
 docker run --name some-postgres-db -p 5432:5432 -e POSTGRES_DB=initdb -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-docker run --name postgres-test-db -p 5433:5432 -e POSTGRES_DB=testdb -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+docker run --name postgres-test-db -p 5433:5432 -e POSTGRES_DB=testdb -e POSTGRES_PASSWORD=mysecretpassword -d postgres -c fsync=off
