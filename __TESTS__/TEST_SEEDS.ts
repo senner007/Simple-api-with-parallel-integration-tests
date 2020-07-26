f9225cd25590e5488d441364819be20a9b63d3c2
nig <nig@beepanalytics.com>
2020-07-26 17:43:36 +0200

    fix too many knex client calls

--- __TESTS__/TEST_SEEDS.ts
+++ __TESTS__/TEST_SEEDS.ts
@@ -2,22 +2,13 @@ import { dbManager } from '../knex-manager';
 
 export async function TEST_SEEDS(thread: number) {
   // TODO : could require all and run sequentially
-  await require('../src/seeds/development/01.roles').seed(
-    dbManager(thread).knexInstance(),
-  );
-  await require('../src/seeds/development/02.users').seed(
-    dbManager(thread).knexInstance(),
-  );
-  await require('../src/seeds/development/03.user-roles').seed(
-    dbManager(thread).knexInstance(),
-  );
-  await require('../src/seeds/development/04.lists').seed(
-    dbManager(thread).knexInstance(),
-  );
-  await require('../src/seeds/development/05.items').seed(
-    dbManager(thread).knexInstance(),
-  );
-  await require('../src/seeds/development/06.list-items').seed(
-    dbManager(thread).knexInstance(),
-  );
+  const knexInstance = dbManager(thread).knexInstance();
+
+  await require('../src/seeds/development/01.roles').seed(knexInstance);
+  await require('../src/seeds/development/02.users').seed(knexInstance);
+  await require('../src/seeds/development/03.user-roles').seed(knexInstance);
+  await require('../src/seeds/development/04.lists').seed(knexInstance);
+  await require('../src/seeds/development/05.items').seed(knexInstance);
+  await require('../src/seeds/development/06.list-items').seed(knexInstance);
+  knexInstance.destroy();
 }
